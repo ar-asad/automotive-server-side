306749864991d5e62af9f5ec7e084748d13198fb
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-19 16:35:06 +0600

    connect server with mongodb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
@@ -13,16 +14,32 @@ app.use(express.json());
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ruibyui.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 async function run() {
     try {
         await client.connect();
-        const userCollection = client.db("foodExpress2").collection("myuser");
+        const toyCarCollection = client.db("toycar").collection("cars");
 
-    }
-    finally {
-        // await client.close()
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
     }
 }
+run().catch(console.dir);
 
 
 
